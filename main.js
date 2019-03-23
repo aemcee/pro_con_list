f087785aaa89c2d058e3d0352b64469515a715d2
Cody Miller <cody.miller@learningfuze.com>
2019-03-23 08:50:30 -0700

    Function template build in main.js

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
 $(document).ready(function(){
-    $(".submit").on("click", handleSubmit)
+
 })
 
 function handleSubmit(event){
-    var input = $(".form-control");
-    var inputVal = input.val();
-    input.val("");
-    createListEntry(inputVal, isPro(inputVal) ? addProToList : addConToList);
+
 }
 
 function isPro(textContent){
-    if(textContent.startsWith("PRO:")){
-        return true;
-    }
-    return false;
+
 }
 
 function createListEntry(elementText, callback){
-    var listEntryElem = $("<p>").text(elementText).on("click", toggleListEntry);
-    callback(listEntryElem);
+
 }
 
 function addProToList($elem){
-    $elem.css({color:"forestgreen"}).appendTo($("#pros"));
+
 }
 
 function addConToList($elem){
-    $elem.css({color:"firebrick"}).appendTo($("#cons"));
+
 }
 
-function toggleListEntry({ currentTarget: elem }){
-    var elemText = $(elem).text();
-    var elemIsPro = isPro(elemText);
-    $(elem).remove();
-    elemText = `${elemIsPro ? "CON:" : "PRO:"}${elemText.slice(4)}`
-    createListEntry(elemText, elemIsPro ? addConToList : addProToList);
+function toggleListEntry(event){
+
 }
